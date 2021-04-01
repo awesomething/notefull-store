be6649f2a3043a31fe3fffccb577a7cac6ae000c
Awesome Henry <henryebomah@gmail.com>
2021-03-31 18:13:28 -0700

    Update postgrator-config.js

--- postgrator-config.js
+++ postgrator-config.js
@@ -14,12 +14,3 @@ module.exports = {
   
 }
 
-/*{
-  "migrationDirectory": "migrations",
-  "driver": "pg",
-  "host": "ec2-52-45-73-150.compute-1.amazonaws.com",
-  "port": 5432,
-  "database": "ddlhb3l5cftjo",
-  "username": "ruukxcstvyvwod",
-  "password": "11486604166a9045c6377d72f12d9e897ef4820c47820c6f6fbb6f754fc56f43"
-}*/
